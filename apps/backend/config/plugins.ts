fb04e61f2a9e5b6ab67630ee63d0aaec95368fa3
Paweł Niedźwiecki <hello@uxu.pl>
2023-01-08 17:57:02 +0100

    feat: add GQL

--- apps/backend/config/plugins.ts
+++ apps/backend/config/plugins.ts
@@ -24,5 +24,17 @@ module.exports = ({env}) => ({
         },
       },
     },
-  }
+  },
+  graphql: {
+    config: {
+      endpoint: "/graphql",
+      shadowCRUD: true,
+      playgroundAlways: false,
+      depthLimit: 20,
+      amountLimit: 100,
+      apolloServer: {
+        tracing: false,
+      },
+    },
+  },
 });
