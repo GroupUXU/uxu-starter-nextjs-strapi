4fd6e64bafab1555adae242caf8b1b3cbb58dad7
Paweł Niedźwiecki <37551231+pawel-niedzwiecki@users.noreply.github.com>
2023-01-21 17:07:59 +0100

    Development (#21)
    * feat: add nx
    
    * fieat: add frontent app and config
    
    * feat: add strapi
    
    * feat: add submodule (#3)
    
    * Feat/development docker (#4)
    
    * feat: add config for strapi
    
    * feat: add cocker for development
    
    * Backend (#11)
    
    * Feat/backend postgresql (#5)
    
    * feat: add postgreSQL
    
    * fix: fix strapi
    
    * Feat/backend postgresql (#6)
    
    * feat: add postgreSQL
    
    * fix: fix strapi
    
    * fix: update .env.example
    
    * Fix/backend sendgrid (#7)
    
    * feat: update .gitignore && add plugin for backend sendgrid
    
    * fix: add config email plugin
    
    * Fix/backend sendgrid (#8)
    
    * feat: update .gitignore && add plugin for backend sendgrid
    
    * fix: add config email plugin
    
    * fix: fix version sendgrid
    
    * feat: add config for aws s3 (#9)
    
    * feat: add GQL (#10)
    
    * Backend (#14)
    
    * Feat/backend postgresql (#5)
    
    * feat: add postgreSQL
    
    * fix: fix strapi
    
    * Feat/backend postgresql (#6)
    
    * feat: add postgreSQL
    
    * fix: fix strapi
    
    * fix: update .env.example
    
    * Fix/backend sendgrid (#7)
    
    * feat: update .gitignore && add plugin for backend sendgrid
    
    * fix: add config email plugin
    
    * Fix/backend sendgrid (#8)
    
    * feat: update .gitignore && add plugin for backend sendgrid
    
    * fix: add config email plugin
    
    * fix: fix version sendgrid
    
    * feat: add config for aws s3 (#9)
    
    * feat: add GQL (#10)
    
    * feat: update gitignore
    
    * Backend (#18)
    
    * Feat/backend postgresql (#5)
    
    * feat: add postgreSQL
    
    * fix: fix strapi
    
    * Feat/backend postgresql (#6)
    
    * feat: add postgreSQL
    
    * fix: fix strapi
    
    * fix: update .env.example
    
    * Fix/backend sendgrid (#7)
    
    * feat: update .gitignore && add plugin for backend sendgrid
    
    * fix: add config email plugin
    
    * Fix/backend sendgrid (#8)
    
    * feat: update .gitignore && add plugin for backend sendgrid
    
    * fix: add config email plugin
    
    * fix: fix version sendgrid
    
    * feat: add config for aws s3 (#9)
    
    * feat: add GQL (#10)
    
    * feat: update gitignore
    
    * feat: add docker for production
    
    * Backend (#19)
    
    * Feat/backend postgresql (#5)
    
    * feat: add postgreSQL
    
    * fix: fix strapi
    
    * Feat/backend postgresql (#6)
    
    * feat: add postgreSQL
    
    * fix: fix strapi
    
    * fix: update .env.example
    
    * Fix/backend sendgrid (#7)
    
    * feat: update .gitignore && add plugin for backend sendgrid
    
    * fix: add config email plugin
    
    * Fix/backend sendgrid (#8)
    
    * feat: update .gitignore && add plugin for backend sendgrid
    
    * fix: add config email plugin
    
    * fix: fix version sendgrid
    
    * feat: add config for aws s3 (#9)
    
    * feat: add GQL (#10)
    
    * feat: update gitignore
    
    * feat: add docker for production
    
    * fix: update .env.example
    
    * fix: fix db config
    
    * fix: update data
    
    * fix: fix Dockerfile
    
    * fix: update docker compose
    
    * fix: update docker
    
    * Backend (#20)
    
    * Feat/backend postgresql (#5)
    
    * feat: add postgreSQL
    
    * fix: fix strapi
    
    * Feat/backend postgresql (#6)
    
    * feat: add postgreSQL
    
    * fix: fix strapi
    
    * fix: update .env.example
    
    * Fix/backend sendgrid (#7)
    
    * feat: update .gitignore && add plugin for backend sendgrid
    
    * fix: add config email plugin
    
    * Fix/backend sendgrid (#8)
    
    * feat: update .gitignore && add plugin for backend sendgrid
    
    * fix: add config email plugin
    
    * fix: fix version sendgrid
    
    * feat: add config for aws s3 (#9)
    
    * feat: add GQL (#10)
    
    * feat: update gitignore
    
    * feat: add docker for production
    
    * fix: update .env.example
    
    * fix: fix db config
    
    * fix: update data
    
    * fix: fix Dockerfile
    
    * fix: update docker compose
    
    * fix: update docker

--- apps/backend/config/database.ts
+++ apps/backend/config/database.ts
@@ -1,3 +1,14 @@
+<<<<<<< HEAD
+module.exports = ({ env }) => ({
+  connection: {
+    client: env.DATABASE_CLIENT,
+    connection: {
+      host: env.DATABASE_HOST,
+      port: env.DATABASE_PORT,
+      database: env.DATABASE_NAME,
+      user: env.DATABASE_USERNAME,
+      password: env.DATABASE_PASSWORD,
+=======
 const parse = require("pg-connection-string").parse;
 const config = parse(process.env.DATABASE_URL);
 
@@ -10,6 +21,7 @@ module.exports = ({ env }) => ({
       database: config.database,
       user: config.user,
       password: config.password,
+>>>>>>> stage
       ssl: {
         rejectUnauthorized: false,
       },
