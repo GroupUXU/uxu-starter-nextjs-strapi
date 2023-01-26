3c6eb290dc45fb58180f7186caf9dd130f8151b7
Paweł Niedźwiecki <37551231+pawel-niedzwiecki@users.noreply.github.com>
2023-01-26 21:04:51 +0100

    Backend (#23)
    * Feat/backend postgresql (#5)
    
    * feat: add postgreSQL
    
    * fix: fix strapi
    
    * Feat/backend postgresql (#6)
    
    * feat: add postgreSQL
    
    * fix: fix strapi
    
    * fix: update .env.example
    
    * Fix/backend sendgrid (#7)
    
    * feat: update .gitignore && add plugin for backend sendgrid
    
    * fix: add config email plugin
    
    * Fix/backend sendgrid (#8)
    
    * feat: update .gitignore && add plugin for backend sendgrid
    
    * fix: add config email plugin
    
    * fix: fix version sendgrid
    
    * feat: add config for aws s3 (#9)
    
    * feat: add GQL (#10)
    
    * feat: update gitignore
    
    * feat: add docker for production
    
    * fix: update .env.example
    
    * fix: fix db config
    
    * fix: update data
    
    * fix: fix Dockerfile
    
    * fix: update docker compose
    
    * fix: update docker

--- apps/backend/config/database.ts
+++ apps/backend/config/database.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 module.exports = ({ env }) => ({
   connection: {
     client: env.DATABASE_CLIENT,
@@ -8,20 +7,6 @@ module.exports = ({ env }) => ({
       database: env.DATABASE_NAME,
       user: env.DATABASE_USERNAME,
       password: env.DATABASE_PASSWORD,
-=======
-const parse = require("pg-connection-string").parse;
-const config = parse(process.env.DATABASE_URL);
-
-module.exports = ({ env }) => ({
-  connection: {
-    client: "postgres",
-    connection: {
-      host: config.host,
-      port: config.port,
-      database: config.database,
-      user: config.user,
-      password: config.password,
->>>>>>> stage
       ssl: {
         rejectUnauthorized: false,
       },
